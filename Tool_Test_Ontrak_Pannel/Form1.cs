9d401aeb08b6288f923a7a23d4c41efa7f7dc36a
hien_pham <blackcastus@gmail.com>
2025-03-28 18:13:12 +0700

    modify all lable name

--- Tool_Test_Ontrak_Pannel/Form1.cs
+++ Tool_Test_Ontrak_Pannel/Form1.cs
@@ -13,23 +13,22 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Data.SqlTypes;
 using System.Web.UI.WebControls.WebParts;
+using Newtonsoft.Json.Linq;
+//using System.Web.UI.WebControls;
 
 namespace Tool_Test_Ontrak_Pannel
 {
     public partial class Form1 : Form
     {
-        readonly double MIN_VOLT3V3 = 3.168;
-        readonly double MAX_VOLT3V3 = 3.432;
-        readonly double MIN_VOLT5V = 4.800;
-        readonly double MAX_VOLT5V = 5.200;
-        readonly double MIN_CURRENT = 2.0;
-        readonly double MAX_CURRENT = 3.2;
         readonly string[] sBaudrate = { "2400", "4800", "9600", "14400", "19200", "38400", "57600", "115200", "128000" };
-
+        private string[] sJsonPaser = {"btp3v", "btp5v", "btpcur", "btp384", "btp2k", "btpis3v","btpis5v","btpiscur", "btpis384", "btpis2k",
+                                        "stp3v", "stp5v", "stp48v", "stp384", "stp2k", "stpis3v", "stpis5v", "stpis48v", "stpis384", "stpis2k",
+                                        "etp3v", "etpcursm", "etpcurtm", "etpis3v", "etpiscursm", "etpiscurtm"};
         private SerialPort mSerialPort;
         private PcbBase mPcbBase03;
         private PcbSync mPcbSync08;
         private PcbEtag mPcbEtag06;
+        private string serialBuffer = "";
         public Form1()
         {
             InitializeComponent();
@@ -45,12 +44,12 @@ namespace Tool_Test_Ontrak_Pannel
             
             // init timer
             Timer timer = new Timer();
-            timer.Interval = 100;
+            timer.Interval = 1000;
             timer.Tick += Timer_Tick;
             timer.Start();
 
             Timer timer_100ms = new Timer();
-            timer_100ms.Interval = 100;
+            timer_100ms.Interval = 50;
             timer_100ms.Tick += Timer_100ms;
             timer_100ms.Start();
         }
@@ -63,6 +62,7 @@ namespace Tool_Test_Ontrak_Pannel
             cboCom.Items.Clear();
             cboCom.Items.AddRange(ports);
             cboBaud.Text = "9600";
+            cboCom.Text = "COM4";
         }
         private void Timer_100ms(object sender, EventArgs e)
         {
@@ -114,24 +114,29 @@ namespace Tool_Test_Ontrak_Pannel
             {
                 if(cboCom.SelectedItem != null)
                 {
+                    // Set port name, baud, parity, databit, stopbit
                     mSerialPort.PortName = cboCom.Text;
                     mSerialPort.BaudRate = int.Parse(cboBaud.Text);
                     mSerialPort.Parity = Parity.None;
                     mSerialPort.DataBits = 8;
                     mSerialPort.StopBits = StopBits.One;
-                    mSerialPort.DataReceived += serialPort1_DataReceived;
                     mSerialPort.Open();
 
-                    Console.WriteLine("Connected with " + cboCom.Text);
-                    // update status connected
+                    // call event data receive
+                    mSerialPort.DataReceived += serialPort1_DataReceived;
+
+                    // update status connect or disconnect
                     if (mSerialPort.IsOpen)
                     {
+                        tbDebug.Text = "Connected with " + cboCom.Text;
+
                         lbComStatus.Text = "Connected";
                         lbComStatus.ForeColor = Color.Green;
-                        //tbRevContent.Text = "Connected with " + cboCom.Text + ", " + cboBaud.Text + Environment.NewLine;
                     }
                     else
                     {
+                        tbDebug.Text = "Disconnected with " + cboCom.Text;
+
                         lbComStatus.Text = "Disconnected";
                         lbComStatus.ForeColor = Color.Red;
                     }
@@ -183,96 +188,169 @@ namespace Tool_Test_Ontrak_Pannel
         private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             // Check triger event
-            // Console.WriteLine("Serial data received!");
-            string serialBuffer = "";
-
-            string DataRev = mSerialPort.ReadExisting();
-            serialBuffer += DataRev;
+            //string tmBuffer = "";
+            string DataRev = mSerialPort.ReadLine();
+            //tmBuffer += DataRev;
             //Console.WriteLine(DataRev);
-
-            if(serialBuffer.StartsWith("{") && serialBuffer.EndsWith("}"))
+            if (DataRev.EndsWith("\r"))
             {
-                try
-                {
-                    var dataJson = JsonSerializer.Deserialize<Dictionary<string, object>>(serialBuffer);
-                    if (dataJson != null)
-                    {
-                        mPcbBase03.SetVolt3V3(double.Parse(dataJson["bvolt3"].ToString()));
-                        mPcbBase03.SetVolt5V(double.Parse(dataJson["bvolt5"].ToString()));
-                        mPcbBase03.SetCurrent(double.Parse(dataJson["bcur"].ToString()));
-                        mPcbBase03.SetSync384(double.Parse(dataJson["bsync38"].ToString()));
-                        mPcbBase03.SetSync2(double.Parse(dataJson["bsync2"].ToString()));
-
-                        mPcbSync08.setVolt3v3(double.Parse(dataJson["svolt3"].ToString()));
-                        mPcbSync08.setVolt5v(double.Parse(dataJson["svolt5"].ToString()));
-                        mPcbSync08.setVolt48v(double.Parse(dataJson["svolt48"].ToString()));
-                        mPcbSync08.setSync384(double.Parse(dataJson["ssync38"].ToString()));
-                        mPcbSync08.setSync2(double.Parse(dataJson["ssync2"].ToString()));
-
-                        mPcbEtag06.SetVolt3v3(double.Parse(dataJson["evolt3"].ToString()));
-                        mPcbEtag06.SetCurrentSleepMode(double.Parse(dataJson["ecursleep"].ToString()));
-                        mPcbEtag06.SetCurrentTransMode(double.Parse(dataJson["ecurtrans"].ToString()));
-                    }
-                }
-                catch(Exception ex)
-                {
-                    Console.WriteLine("JSON Parse Error: " + ex.Message);
-                }
-                serialBuffer = ""; // clean buffer
+                serialBuffer = DataRev;
+                //tmBuffer = "";
+                this.Invoke(new Action(ParseSerialData)); // Gọi hàm Parse trên UI thread
             }
-            
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
+            // show base value
             tboBase3.Text = mPcbBase03.GetVolt3V3().ToString();
             tboBase5.Text = mPcbBase03.GetVolt5V().ToString();
             tboBaseCur.Text = mPcbBase03.GetCurrent().ToString();
             tboBase384.Text = mPcbBase03.GetSync384().ToString();
             tboBase2.Text = mPcbBase03.GetSync2().ToString();
 
+            // show sync value
             tboSync3.Text = mPcbSync08.GetVolt3v3().ToString();
             tboSync5.Text = mPcbSync08.GetVolt5v().ToString();
             tboSync48.Text = mPcbSync08.GetVolt48v().ToString();
             tboSync384.Text = mPcbSync08.GetSync384().ToString();
             tboSync2.Text = mPcbSync08.GetSync2().ToString();
 
+            // show etag value
             tboEtag3.Text = mPcbEtag06.GetVolt3v3().ToString();
             tboEtagCurSleep.Text = mPcbEtag06.GetCurrentSleepMode().ToString();
             tboEtagCurTrans.Text = mPcbEtag06.GetCurrentTransMode().ToString();
 
-            //if (mPcbBase03.mVolt3v3 >= MIN_VOLT3V3 && mPcbBase03.mVolt3v3 <= MAX_VOLT3V3)
-            //{
-            //    lbBase3.Text = "PASS";
-            //    lbBase3.BackColor = Color.Green;
-            //}
-            //else
-            //{
-            //    lbBase3.Text = "FAIL";
-            //    lbBase3.BackColor = Color.Red;
-            //}
+            if (mPcbBase03.GetIsVolt3v3())
+            {
+                lbBase3.Text = "PASS";
+                lbBase3.BackColor = Color.Green;
+            }
+            else
+            {
+                lbBase3.Text = "FAIL";
+                lbBase3.BackColor = Color.Red;
+            }
 
-            //if (mPcbBase03.mVolt5v >= MIN_VOLT5V && mPcbBase03.mVolt5v <= MAX_VOLT5V)
-            //{
-            //    lbBase5.Text = "PASS";
-            //    lbBase5.BackColor = Color.Green;
-            //}
-            //else
-            //{
-            //    lbBase5.Text = "FAIL";
-            //    lbBase5.BackColor = Color.Red;
-            //}
+            if (mPcbBase03.GetIsVolt5v())
+            {
+                lbBase5.Text = "PASS";
+                lbBase5.BackColor = Color.Green;
+            }
+            else
+            {
+                lbBase5.Text = "FAIL";
+                lbBase5.BackColor = Color.Red;
+            }
 
-            //if (mPcbBase03.mCurrent >= MIN_CURRENT && mPcbBase03.mCurrent <= MAX_CURRENT)
-            //{
-            //    lbBaseCur.Text = "PASS";
-            //    lbBaseCur.BackColor = Color.Green;
-            //}
-            //else
-            //{
-            //    lbBaseCur.Text = "FAIL";
-            //    lbBaseCur.BackColor = Color.Red;
-            //}
+            if (mPcbBase03.GetIsCurrent())
+            {
+                lbBaseCur.Text = "PASS";
+                lbBaseCur.BackColor = Color.Green;
+            }
+            else
+            {
+                lbBaseCur.Text = "FAIL";
+                lbBaseCur.BackColor = Color.Red;
+            }
+
+            if (mPcbBase03.GetIsSync384())
+            {
+                lbBase384.Text = "PASS";
+                lbBase384.BackColor = Color.Green;
+            }
+            else
+            {
+                lbBase384.Text = "FAIL";
+                lbBase384.BackColor = Color.Red;
+            }
+
+            if (mPcbBase03.GetIsSync2())
+            {
+                lbBase2.Text = "PASS";
+                lbBase2.BackColor = Color.Green;
+            }
+            else
+            {
+                lbBase2.Text = "FAIL";
+                lbBase2.BackColor = Color.Red;
+            }
+        }
+
+        private void ParseSerialData()
+        {
+            if (serialBuffer.EndsWith("\r"))
+            {
+                try
+                {
+                    var dataJson = JsonSerializer.Deserialize<Dictionary<string, object>>(serialBuffer);
+                    if (dataJson != null)
+                    {
+                        bool isEnable = false;
+                        foreach (var dic in dataJson)
+                        {
+                            //Console.WriteLine(dic.Key);
+                            if (sJsonPaser.Contains(dic.Key))
+                            {
+                                switch (dic.Key)
+                                {
+                                    case "btp3v":
+                                        mPcbBase03.SetVolt3V3(double.Parse(dic.Value.ToString()));
+                                        break;
+                                    case "btp5v":
+                                        mPcbBase03.SetVolt5V(double.Parse(dic.Value.ToString()));
+                                        break;
+                                    case "btpcur":
+                                        mPcbBase03.SetCurrent(double.Parse(dic.Value.ToString()));
+                                        break;
+                                    case "btp384":
+                                        mPcbBase03.SetSync384(double.Parse(dic.Value.ToString()));
+                                        break;
+                                    case "btp2k":
+                                        mPcbBase03.SetSync2(double.Parse(dic.Value.ToString()));
+                                        break;
+                                    case "btpis3v":
+                                        isEnable = bool.Parse(dic.Value.ToString());
+                                        mPcbBase03.SetIsVolt3v3(isEnable);
+                                        break;
+                                    case "btpis5v":
+                                        isEnable = bool.Parse(dic.Value.ToString());
+                                        mPcbBase03.SetIsVolt5v(isEnable);
+                                        break;
+                                    case "btpiscur":
+                                        isEnable = bool.Parse(dic.Value.ToString());
+                                        mPcbBase03.SetIsCurrent(isEnable);
+                                        break;
+                                    case "btpis384":
+                                        isEnable = bool.Parse(dic.Value.ToString());
+                                        mPcbBase03.SetIsSync384(isEnable);
+                                        break;
+                                    case "btpis2k":
+                                        isEnable = bool.Parse(dic.Value.ToString());
+                                        mPcbBase03.SetIsSync2(isEnable);
+                                        break;
+                                    case "stp3v":
+                                        break;
+                                    case "stp5v":
+                                        break;
+                                    case "stp48v":
+                                        break;
+                                    case "stp384":
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                //}
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("JSON Parse Error: " + ex.Message);
+                }
+                serialBuffer = ""; // clean buffer
+            }
         }
     }
 }
--- Tool_Test_Ontrak_Pannel/PcbBase.cs
+++ Tool_Test_Ontrak_Pannel/PcbBase.cs
@@ -13,6 +13,11 @@ namespace Tool_Test_Ontrak_Pannel
         private double mCurrent;
         private double mSync384;
         private double mSync2;
+        private bool mIsVolt3v3;
+        private bool mIsVolt5v;
+        private bool mIsCurrent;
+        private bool mIsSync384;
+        private bool mIsSync2;
         public PcbBase()
         {
             ResetVariable();
@@ -25,14 +30,12 @@ namespace Tool_Test_Ontrak_Pannel
             mCurrent = 0;
             mSync384 = 0;
             mSync2 = 0;
+            mIsVolt3v3 = false;
+            mIsVolt5v = false;
+            mIsCurrent = false;
+            mIsSync384 = false;
+            mIsSync2 = false;
         }
-
-        // Getter value each value
-        public double GetVolt3V3() => mVolt3v3;
-        public double GetVolt5V() => mVolt5v;
-        public double GetCurrent() => mCurrent;
-        public double GetSync384() => mSync384;
-        public double GetSync2() => mSync2;
 
         // Setter - Cập nhật giá trị
         public void SetVolt3V3(double value) => mVolt3v3 = value;
@@ -40,6 +43,23 @@ namespace Tool_Test_Ontrak_Pannel
         public void SetCurrent(double value) => mCurrent = value;
         public void SetSync384(double value) => mSync384 = value;
         public void SetSync2(double value) => mSync2 = value;
+        public void SetIsVolt3v3(bool value) => mIsVolt3v3 = value;
+        public void SetIsVolt5v(bool value) => mIsVolt5v = value;
+        public void SetIsCurrent(bool value) => mIsCurrent = value;
+        public void SetIsSync384(bool value) => mIsSync384 = value;
+        public void SetIsSync2(bool value) => mIsSync2 = value;
+        // Getter value each value
+        public double GetVolt3V3() => mVolt3v3;
+        public double GetVolt5V() => mVolt5v;
+        public double GetCurrent() => mCurrent;
+        public double GetSync384() => mSync384;
+        public double GetSync2() => mSync2;
+        public bool GetIsVolt3v3() => mIsVolt3v3;
+        public bool GetIsVolt5v() => mIsVolt5v;
+        public bool GetIsCurrent() => mIsCurrent;
+        public bool GetIsSync384() => mIsSync384;
+        public bool GetIsSync2() => mIsSync2;
+
 
     }
 }
